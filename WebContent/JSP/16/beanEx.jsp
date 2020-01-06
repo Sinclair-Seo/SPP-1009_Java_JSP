7ce6ce2840907869914e49708f0a5e0f6697d883
Sinclair Seo <sinclairseo@gmail.com>
2020-01-06 17:15:49 +0900

    16강 자바 빈

---
+++ WebContent/JSP/16/beanEx.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<jsp:useBean id="student" class="com.javalec.ex.Student" scope="page" />
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>16강 자바 빈</title>
+</head>
+<body>
+
+	<h1>16강 자바 빈</h1><br/>
+	
+	<jsp:setProperty name="student" property="name" value="sinclair" />
+	<jsp:setProperty name="student" property="age" value="16" />
+	<jsp:setProperty name="student" property="grade" value="99" />
+	<jsp:setProperty name="student" property="studentNum" value="938" />
+	
+	Name: <jsp:getProperty property="name" name="student"/><br/>
+	Age: <jsp:getProperty property="age" name="student"/><br/>
+	Grade: <jsp:getProperty property="grade" name="student"/><br/>
+	Num: <jsp:getProperty property="studentNum" name="student"/><br/>
+	
+</body>
+</html>
---
+++ src/com/javalec/ex/Student.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.javalec.ex;
+
+/**
+ * @author Sinclair
+ *
+ */
+public class Student {
+
+	private String name;
+	private int age;
+	private int grade;
+	private int studentNum;
+
+	/**
+	 * 
+	 */
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the grade
+	 */
+	public int getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	/**
+	 * @return the studentNum
+	 */
+	public int getStudentNum() {
+		return studentNum;
+	}
+
+	/**
+	 * @param studentNum the studentNum to set
+	 */
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+
+}
